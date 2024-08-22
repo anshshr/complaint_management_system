31cf80868fd1bb9fefec58b722d78108561e9244
Shubham-2004 <shubhambera100@gmail.com>
2024-08-23 00:49:10 +0530

    presentation add commit

--- lib/components/pages/home_page.dart
+++ lib/components/pages/home_page.dart
@@ -9,9 +9,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('home page'),
       ),
-      body: const Center(
-        child: Text('Home page'),
-      ),
+      body:const Center(child:  Text('Home page'),),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
