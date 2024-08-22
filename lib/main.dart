2129c52bb8901a83ee13cee0bb98e8e2222cbf8d
Shubham-2004 <shubhambera100@gmail.com>
2024-08-22 22:51:11 +0530

    modify_main.dart commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,22 @@ import 'package:complaint_management_system/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:  HomePage(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
