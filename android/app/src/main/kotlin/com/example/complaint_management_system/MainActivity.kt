fa2ccff13b56283ec0e11ff3102e0bea85ae953e
anshshr <anshshrivastav24@gmail.com>
2024-08-22 20:47:10 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/complaint_management_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.complaint_management_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:complaint_management_system/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:  HomePage(),
+  ));
+}
