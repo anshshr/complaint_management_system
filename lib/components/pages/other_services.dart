9e5d17bca952dd8f3a85fa01f7d46899c9e2d1f3
anshshr <anshshrivastav24@gmail.com>
2024-08-23 12:57:16 +0530

    new changes

--- lib/components/pages/other_services.dart
+++ lib/components/pages/other_services.dart
@@ -39,8 +39,8 @@ class OtherServicesPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Other Services"),
-        leading: Icon(Icons.arrow_back),
+        title: const Text("Other Services"),
+        leading:const Icon(Icons.arrow_back),
         backgroundColor: Colors.blue,
         actions: [
           Padding(
@@ -51,7 +51,7 @@ class OtherServicesPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
+        child:  GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             crossAxisSpacing: 16.0,
