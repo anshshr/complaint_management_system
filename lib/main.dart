96b2cd3f97d55889a4752f0b2373ffbb5c7c6e1d
Shubham-2004 <shubhambera100@gmail.com>
2024-08-23 13:55:47 +0530

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // import 'package:complaint_management_system/components/pages/home_page.dart';
-import 'package:complaint_management_system/components/pages/home_page.dart';
-import 'package:complaint_management_system/components/pages/other_services.dart';
+import 'package:complaint_management_system/components/app/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
