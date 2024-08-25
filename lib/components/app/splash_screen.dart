397017b85ad0cf765c2c8bfcfd76fa18c640ade7
Shubham-2004 <shubhambera100@gmail.com>
2024-08-25 22:30:09 +0530

    Adding the Rail Madad icon and Name

--- lib/components/app/splash_screen.dart
+++ lib/components/app/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => IntroductoryPages()),
       );
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rail Madad',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
